336963fd205679fd13fd0dcf0dad28e50424fdd9
Rômulo Rocha <romulorochabr@hotmail.com>
2017-04-06 21:11:53 +1200

    Adding verification for messages

--- start.js
+++ start.js
@@ -8,17 +8,54 @@ var bot = controller.spawn({
     token: process.env.slacktoken
 }).startRTM();
 
-// give the bot something to listen for.
-controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
-   bot.reply(message,'Hello yourself.');
-});
+
+controller.on('ambient',function(bot,message) {
+    
+    // collect add "description of the insight"
+    // collect list -> Retrives the ideas
+    // collect remove index -> mark as moved to any project/channel
+
+    // todo add "description of todo" [priority] [user]
+    // todo list -> list channels todos
+    // todo list all -> list all channels todos
+    // todo list done -> list all todos already done
+    // todo remove|tick|strike index
+
+    // COLLECT ACTIONS
+    if(message.text.startsWith("collect add")){
+      bot.reply(message, "New collect added!")
+    }
+    if(message.text.startsWith("collect list")){
+      bot.reply(message, "List of collects")
+    }
+    if(message.text.startsWith("collect remove")){
+      bot.reply(message, "Remove collect")
+    }
+    
+    // TODOs ACTIONS
+    if(message.text.startsWith("todo add")){
+      bot.reply(message, "New TODO added!")
+    }
+    if(message.text.startsWith("todo list")){
+      bot.reply(message, "List of TODOs")
+    }
+    if(message.text.startsWith("todo remove")){
+      bot.reply(message, "Remove TODO")
+    }
+
+    /*bot.reply(message,{
+      text: "A more complex response for: " + JSON.stringify(message),
+      username: "@morpheus",
+      icon_emoji: ":dash:",
+    });*/
+
+})
 
 controller.on('channel_created', function(bot, message) {
 
     request.post({ url: 'https://slack.com/api/users.list', form: { token: process.env.slacktoken} }, function(err, res, body) {
       var bodyjson = eval('(' + body + ')')
-      //var channelid = bodyjson.channel.id;
-      //var members = ['haoyang', 'martina', 'romulo', 'kevin', 'bruce'];
+      
       var members = bodyjson.members
       _.each(members, function(member) {
         request.post({ url: 'https://slack.com/api/channels.invite', form: { token: process.env.slacktokenromulo, channel: message.channel.id, user: member.id} },
@@ -29,33 +66,8 @@ controller.on('channel_created', function(bot, message) {
                   channel: message.channel.id
               }
             );
-
           })
-        /*bot.say(
-          {
-              text: 'The channel ' + message.channel.name + ' was created and ' + member.real_name + ' was invited',
-              channel: 'C4U0W08QP'
-          }
-        );*/
-
       });
-      //request.post({ url: 'https://slack.com/api/channels.setTopic', form: { token: users.haoyang.slacktoken, channel: channelid, topic: purpose } });
-      //request.post({ url: 'https://slack.com/api/channels.setPurpose', form: { token: users.haoyang.slacktoken, channel: channelid, topic: purpose } });
     });
 
   });
-
-// U4T618D26 C4U0W08QP
-//
-/*
-controller.on('ambient',function(bot,message) {
-
-      bot.reply(message,{
-              text: "A more complex response",
-               username: "ReplyBot",
-              icon_emoji: ":dash:",
-      });
-
-})
-
-*/
